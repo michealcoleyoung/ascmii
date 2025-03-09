d76b8327dd80b51b8fc9ebd3032b9098bb006b6c
michealcoleyoung <coleyoungmusic@gmail.com>
2025-03-08 18:07:47 -0600

    adding files

---
+++ ascmii.py
@@ -0,0 +1,12 @@
+from ascii_magic import AsciiArt, from_image
+
+# my_art = AsciiArt.from_image('cross-surreal.jpg')
+# my_art.to_terminal()
+
+
+def main():
+    print("Hello")
+    print("This is the second text print statement")
+
+if __name__ == '__main__':
+    main()
