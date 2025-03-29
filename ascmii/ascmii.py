994b984e2106cf2d71f4c17c9697c9334834913b
michealcoleyoung <coleyoungmusic@gmail.com>
2025-03-29 16:53:44 -0500

    started from scratch and added width, char and monochrome arguments

--- ascmii/ascmii.py
+++ ascmii/ascmii.py
@@ -25,74 +25,63 @@ d88P     888  "Y8888P"   "Y8888P"  888       888 8888888 8888888
     print(ascmii_doc.__doc__)
 
 def convert_to_ascii(img_path, **kwargs):
-    try:
-        art = AsciiArt.from_image(img_path)
-        output = art.to_ascii(**kwargs)
-        print(output)
-    except FileNotFoundError:
-        print(f"Error: Image file was not found at {img_path}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    art = AsciiArt.from_image(img_path)
+    output = art
 
+    # if 'width' in kwargs:
+    #     width = kwargs['width']
+    #     output = art.to_ascii(width)
+    #     print('Width argument is being used')
+    # else:
+    #     output = art.to_ascii(**kwargs)
+    # print(output)
 
+    for key, value in kwargs.items():
+        if key == 'width':
+            output = art.to_ascii(value)
+            print('Width argument is being passed')
+        elif key == 'char':
+            output = art.to_ascii(char=value)
+            print('Char argument is being passed')
+        elif key == 'monochrome':
+            output = art.to_ascii(monochrome=True)
+            print('Monochrome argument is being passed')
+    print(output)
+    
 def convert_to_html(img_path, **kwargs):
-    pass
+    art = AsciiArt.from_image(img_path)
+    output = art.to_html_file('art_html_test.html', columns=200, width_ratio=2)
+    print(output)
+    
+    if output:
+        print("HTML file was successfully created")
+        
 
-def main():
-    parser = argparse.ArgumentParser(usage="ascmii image.png --color", description=ascmii_doc())
-    parser.add_argument("image", help="Path to the input image file.")
-    parser.add_argument("--width", type=int, default=120, help="Width of the output ASCII art (default: 120).")
-    parser.add_argument("--color", action="store_true", help="Enable color output.")
-    parser.add_argument("--output", help="Path to save the ASCII art to a file.")
-    parser.add_argument("--char-set", default=None, help="Specify a character to use for the ASCII art.")
-    parser.add_argument("--monochrome", action="store_true", help="Enable grayscale (monochrome) output.")
-    parser.add_argument("--back", type=str, default=None, help="Background Color.")
-    parser.add_argument("--front", type=str, default=None, help="Foreground Color.")
-    # add_argument for --html
-
-
-    args = parser.parse_args()
-
-    kwargs = {
-            'columns': args.width,
-            'monochrome': args.monochrome,
-            }
 
    
-    if args.char_set:
-        kwargs['char'] = args.char_set
-
-
-    if args.color and args.monochrome:
-        print("Error: Cannot use both --color and --monochrome options together.")
-        sys.exit(1)
-
-    if args.front:
-        try:
-            kwargs['front'] = Front[args.front.upper()]
-        except KeyError:
-            print("Error: Invalid Front Color.")
-            sys.exit(1)
+def convert_to_file(img_path, **kwargs):
+    pass
     
-    if args.back:
-        try:
-            kwargs['back'] = Back[args.back.upper()]
-        except KeyError:
-            print("Error: Invalid Back Color.")
-            sys.exit(1)
+def main():
+    parser = argparse.ArgumentParser(usage='ascmii image.png --color', description=ascmii_doc())
+    parser.add_argument('image', help='Path to the input image file')
+    parser.add_argument('--width', type=int, default=120, help='Width of the output ASCII art (default: 120)')
+    parser.add_argument('--char', type=str, help='Specify a character to use for the ASCII art')
+    parser.add_argument('--monochrome', action='store_true', help='Sets ASCII art to gray')
+    args = parser.parse_args()
 
+    kwargs = {}
 
-    ascii_art = AsciiArt.from_image(args.image).to_ascii(**kwargs)
+    if args.width:
+        kwargs['width'] = args.width
+    if args.char:
+        kwargs['char'] = args.char
+    if args.monochrome:
+        kwargs['monochrome'] = args.monochrome
+        
+    convert_to_ascii(args.image, **kwargs)
 
-    if args.output:
-        try:
-            with open(args.output, 'w') as f:
-                f.write(ascii_art)
-            print(f"ASCII art saved to {args.output}")
-        except Exception as e:
-            print(f"Error saving to file: {e}")
-    else:
-        print(ascii_art)
-
+    
+    
 if __name__ == '__main__':
     main()
