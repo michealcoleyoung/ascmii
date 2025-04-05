10c2e1e6201e10473d4024beea12ab64d488edc1
michealcoleyoung <coleyoungmusic@gmail.com>
2025-04-04 21:34:26 -0500

    Simplified the convert_to_ascii function based on to_ascii object

--- ascmii/ascmii.py
+++ ascmii/ascmii.py
@@ -26,28 +26,9 @@ d88P     888  "Y8888P"   "Y8888P"  888       888 8888888 8888888
 
 def convert_to_ascii(img_path, **kwargs):
     art = AsciiArt.from_image(img_path)
-    output = art
-
-    # if 'width' in kwargs:
-    #     width = kwargs['width']
-    #     output = art.to_ascii(width)
-    #     print('Width argument is being used')
-    # else:
-    #     output = art.to_ascii(**kwargs)
-    # print(output)
-
-    for key, value in kwargs.items():
-        if key == 'width':
-            output = art.to_ascii(value)
-            print('Width argument is being passed')
-        elif key == 'char':
-            output = art.to_ascii(char=value)
-            print('Char argument is being passed')
-        elif key == 'monochrome':
-            output = art.to_ascii(monochrome=True)
-            print('Monochrome argument is being passed')
+    output = art.to_ascii(**kwargs)
     print(output)
-    
+ 
 def convert_to_html(img_path, **kwargs):
     art = AsciiArt.from_image(img_path)
     output = art.to_html_file('art_html_test.html', columns=200, width_ratio=2)
@@ -55,30 +36,45 @@ def convert_to_html(img_path, **kwargs):
     
     if output:
         print("HTML file was successfully created")
-        
-
 
    
 def convert_to_file(img_path, **kwargs):
     pass
-    
+
+
+"""
+REFERENCE FOR THE TO_ASCII OBJECT
+AsciiArt.to_ascii(
+    columns: int = 120,
+    width_ratio: float = 2.2,
+    monochrome: bool = False,
+    char: Optional[str],
+    front: Optional[Front],
+    back: Optional[Back]
+) -> str
+"""
+
+
 def main():
     parser = argparse.ArgumentParser(usage='ascmii image.png --color', description=ascmii_doc())
     parser.add_argument('image', help='Path to the input image file')
-    parser.add_argument('--width', type=int, default=120, help='Width of the output ASCII art (default: 120)')
-    parser.add_argument('--char', type=str, help='Specify a character to use for the ASCII art')
+    parser.add_argument('--columns', type=int, default=0, help='The number of characters per row, more columns = wider art')
+    parser.add_argument('--width', type=float, default=0, help='Width of the output ASCII art (default: 2.2)')
     parser.add_argument('--monochrome', action='store_true', help='Sets ASCII art to gray')
+    parser.add_argument('--char', type=str, help='Specify a character to use for the ASCII art')
+    # front
+    # back
     args = parser.parse_args()
 
     kwargs = {}
 
-    if args.width:
-        kwargs['width'] = args.width
+    if args.columns:
+        kwargs['columns'] = args.columns
     if args.char:
-        kwargs['char'] = args.char
+        kwargs['width'] = args.width
     if args.monochrome:
         kwargs['monochrome'] = args.monochrome
-        
+            
     convert_to_ascii(args.image, **kwargs)
 
     
